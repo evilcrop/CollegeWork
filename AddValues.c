f5fca1c65bf7b0078841eea88a1a1616af8ef226
Waseem Akram <94538968+evilcrop@users.noreply.github.com>
2022-08-17 10:08:37 +0500

    Add files via upload

---
+++ AddValues.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main (){
+    int a = 2;
+    int b = 6;
+    printf("The Sum Of is = %d\n", a + b);
+    return 0;
+}
---
+++ inputoutput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("Enter First Value : ");
+    scanf("%d", &a);
+    printf("Enter Second Value : ");
+    scanf("%d", &b);
+    printf("The Sum Of The Values is = %d \n", a + b);
+    return 0;
+}
